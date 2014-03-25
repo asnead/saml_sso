20e8328b3eb14232e1cfd6fc7d334ea4c5819c92
Alabê Duarte <aduarte@thoughtworks.com>
2014-03-25 17:14:14 -0300

    +rcaval - working example with passport-saml

--- okta-node-express/auth.js
+++ okta-node-express/auth.js
@@ -1,29 +1,65 @@
-var passport = require('passport');
-var config = require('./config.json');
+var passport = require('passport'),
+  SamlStrategy = require('passport-saml').Strategy,
+  config = require('./config.json');
 
-exports.initalize = passport.initalize;
-exports.session = passport.session;
-exports.authenticate = passport.authenticate;
+var users = [];
 
+function findByEmail(email, fn) {
+  for (var i = 0, len = users.length; i < len; i++) {
+    var user = users[i];
+    if (user.email === email) {
+      return fn(null, user);
+    }
+  }
+  return fn(null, null);
+}
 
-exports.protected = function protected(req, res, next) {
-  if (req.isAuthenticated()) { return next(); }
-  res.redirect('/login')
-};
+// Passport session setup.
+//   To support persistent login sessions, Passport needs to be able to
+//   serialize users into and deserialize users out of the session.  Typically,
+//   this will be as simple as storing the user ID when serializing, and finding
+//   the user by ID when deserializing.
+passport.serializeUser(function(user, done) {
+  done(null, user.email);
+});
+
+passport.deserializeUser(function(id, done) {
+  findByEmail(id, function (err, user) {
+    done(err, user);
+  });
+});
 
 passport.use(new SamlStrategy(
-  { 
+  {
+    issuer: "http://localhost:3000/",
   	path: '/login/callback',
-    entryPoint: config.auth.target_url,
-    protocol: 'https://',
-    cert: config.auth.fingerprint
+    entryPoint: config.auth.entryPoint,
+    cert: config.auth.cert
   },
   function(profile, done) {
-    findByEmail(profile.email, function(err, user) {
-      if (err) {
-        return done(err);
-      }
-      return done(null, user);
+    if (!profile.email) {
+      return done(new Error("No email found"), null);
+    }
+    process.nextTick(function () {
+      findByEmail(profile.email, function(err, user) {
+        if (err) {
+          return done(err);
+        }
+        if (!user) {
+          users.push(profile);
+          return done(null, profile);
+        }
+        return done(null, user);
+      })
     });
-  })
+  }
 ));
+
+passport.protected = function protected(req, res, next) {
+  if (req.isAuthenticated()) {
+    return next();
+  }
+  res.redirect('/login');
+};
+
+ exports = module.exports = passport;
